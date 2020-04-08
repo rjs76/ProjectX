75bc6db6880356d9d23e38412dc81246c7b8f1a7
mwagg22 <47122152+mwagg22@users.noreply.github.com>
2020-04-08 15:14:41 -0400

    Add files via upload

---
+++ Assets/Scenes/scripts/scenebutton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/** 
+ * Holds data and handles the process for changing sccenes between buttons. 
+ * @author Riviere Seguie
+ */
+
+public class scenebutton : MonoBehaviour
+{
+    /** 
+ * Will take you to the specified unity scene
+ */
+    public void Startmenu()
+    {
+        SceneManager.LoadScene("Startmenu");
+    }
+    /** 
+    * Will take you to the specified unity scene
+    */
+    public void secondmenu()
+    {
+        SceneManager.LoadScene("secondmenu");
+    }
+    /** 
+    * Will take you to the specified unity scene
+    */
+    public void thirdmenu()
+    {
+        SceneManager.LoadScene("thirdmenu");
+    }
+    /** 
+    * Will take you to the specified unity scene
+    */
+    public void loginmenu()
+    {
+        SceneManager.LoadScene("loginmenu");
+    }
+    /** 
+    * Will take you to the specified unity scene
+    */
+    public void statsmenu()
+    {
+        SceneManager.LoadScene("statsmenu");
+    }
+    /** 
+    * Will quit the application or game when used
+    */
+    public void QuitNOW()
+    {
+        Application.Quit();
+    }
+    /** 
+    * Will take you to the specified unity scene
+    */
+    public void Matchup()
+    {
+        SceneManager.LoadScene("Match");
+    }
+}
